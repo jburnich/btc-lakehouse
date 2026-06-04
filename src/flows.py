d1b0e509acea2f495597d96b73c686a3cb6f2124
jburnich <burnichonjordan@gmail.com>
2026-06-04 19:10:31 +0200

    fix: enable Prefect logging

--- src/flows.py
+++ src/flows.py
@@ -7,22 +7,22 @@ from cli.sync_tables import main as sync_tables_main
 from cli.ingest import main as _ingest_main
 
 
-@task
+@task(log_prints=True)
 def sync_tables() -> None:
     sync_tables_main()
 
 
-@task
+@task(log_prints=True)
 def ingest(date_str: str) -> None:
     sys.argv = ["ingest", date_str]
     _ingest_main()
 
 
-@flow
+@flow(log_prints=True)
 def sync_tables_flow() -> None:
     sync_tables()
 
 
-@flow
+@flow(log_prints=True)
 def ingest_flow(date_str: str = str(date.today() - timedelta(days=1))) -> None:
     ingest(date_str)
